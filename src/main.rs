b9926b80caa560463fa574e126a75910016cae4d
panch0us <godbryansk@yandex.ru>
2023-04-22 22:24:12 +0300

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
 
 fn main() {
     let text = String::from("Фамилия: Иванов Имя: Иван Отчество: Иванович");
@@ -10,4 +13,15 @@ fn main() {
     for word in text_split {
         println!("{}", word);
     }
+
+    let file = File::open("file.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    for (index, line) in reader.lines().enumerate() {
+        let line = line.unwrap(); // Ignore errors.
+        // Show the line and its number.
+        println!("{}. {}", index + 1, line);
+    }
 }
+
+//иванов;иван;иванович;2222;;
