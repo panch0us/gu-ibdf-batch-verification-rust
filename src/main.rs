69bf13baaa89ec6e2c4b57fafa6310b1f18d1cc3
panch0us <godbryansk@yandex.ru>
2023-04-22 22:13:17 +0300

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+
 fn main() {
-    println!("Hello, world!");
+    let text = String::from("Фамилия: Иванов Имя: Иван Отчество: Иванович");
+    let text_split: Vec<&str> = text.split(" ").collect();
+
+    println!("{}", text);
+
+    println!("{:?}", text_split);
+
+    for word in text_split {
+        println!("{}", word);
+    }
 }
